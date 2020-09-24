23151489933d622354006c1a99e03090249f68ac
Andrew Thyng <andrew@thyng.dev>
2020-09-23 22:23:21 -0500

    Remove console logs

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { SafeAreaView, StatusBar, StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native'
 import { Chat } from './src/components/Chat/Chat'
 
 const App = () => {
-  console.log(StatusBar.currentHeight)
   return (
     <SafeAreaView
-      style={{flex: 1, backgroundColor: '#ECF4F6'}}
+      style={{ flex: 1, backgroundColor: '#ECF4F6' }}
     >
       <Chat
         user='Andrew'
